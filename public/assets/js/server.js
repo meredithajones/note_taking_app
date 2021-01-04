32e3678bdd78b520f1c73bd489679d940d98be08
Meredith Jones <mj19003@gmail.com>
2021-01-04 17:28:03 -0500

    Updating with server.js psudocoding

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,12 +1,14 @@
+// Dependencies
+
 var express= require ('express');
 
+// Sets up the Express App
 var app = express();
 
-//HTML Routes need to be created: get/notes will return the notes.html file
-//get * will return the index.html file
+//Using process.env.port to enable heroku to select a port # is 3001 is not available. 
+var PORT = process.env.PORT || 3001;
 
-//create API routes:
-//Get /api/notes will read the db.json file and return all the saved notes as JSON
+
 //Post /api/notes should receive a new note and save it on the 
 //request body, add it to db.json file & return new note to the client.
 //Delete /api/notes/:id - should recieve a query parameter containing the id of the 
@@ -14,3 +16,39 @@ var app = express();
 //In order to delete, you will need to read all notes from the db.json
 //remove the note wiht the given id property, and then rewrite the 
 //notes to the db.json file. 
+
+// Setting up Express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routes
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+//HTML Routes:
+// get/notes will return the notes.html file
+//get * will return the index.html file
+
+app.get("/notes",(req, res) => 
+  res.sendFile(path.join(__dirname, "notes.html")));
+
+
+app.get("/*", (req, res) => 
+    res.sendFile(path.join(__dirname, "index.html")));
+
+//Creating API routes:
+//Get /api/notes will read the db.json file and return all the saved notes as JSON
+
+app.get("/api/notes", (req,res) =>
+    return res.json(notes);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
